f839ba5807ae1bad469d9db663bf9b91021bc869
Utaka19 <185202718+Utaka19@>
2025-11-16 16:56:38 +0900

    オブジェクトの中にオブジェクトを入れる

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,8 @@ console.log(price.value)
 function increment() {
   price.value++
   instructor.age++
+  instructor.bio = 'hi'
+  instructor.sns.twitter = 'hello'
 }
 const info = ref({
     students: 1000,
@@ -13,9 +15,15 @@ const info = ref({
 })
 const instructor = reactive({
     name: 'Sei',
-    age: 25
+    age: 25,
+    sns: {
+        twitter: '@sei',
+        youtube: '@sei_channel'
+    }
 })
+instructor.bio = 'hello'
 console.log(instructor.age)
+console.log(instructor.sns.twitter)
 </script>
 <template>
   <h1>Title: {{ title }}</h1>
@@ -23,4 +31,6 @@ console.log(instructor.age)
   <button @click="increment">button</button>
   <h2>Students: {{ info.students }}</h2>
   <h2>Instructor age: {{ instructor.age }}</h2>
+  <h2>Instructor bio: {{ instructor.bio }}</h2>
+  <h2>Instructor SNS Twitter: {{ instructor.sns.twitter }}</h2>
 </template>
