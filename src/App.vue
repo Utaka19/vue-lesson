f3c45729c6c333c372511be775501c839aaa39b0
Utaka19 <185202718+Utaka19@>
2025-11-16 11:24:18 +0900

    オブジェクトをリアクティブにする場合

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <script setup>
 import { ref } from 'vue'
-const title = 'Vue.jd Course'
+const title = ref('Vue.jd Course')
 let price = ref(9.99)
 console.log(price.value)
 function increment() {
   price.value++
   console.log(price.value)
 }
+const info = ref({
+    students: 1000,
+    rating: 4.0
+})
+console.log(info.value.students)
 </script>
 <template>
   <h1>Title: {{ title }}</h1>
   <h2>Price: ${{ price - 1 }}</h2>
   <button @click="increment">button</button>
+  <h2>Students: {{ info.students }}</h2>
 </template>
