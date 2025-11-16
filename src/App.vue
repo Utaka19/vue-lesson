af8f76ee7b280ef0e5f1d30f277aac0cbb3a277a
Utaka19 <185202718+Utaka19@>
2025-11-16 16:45:25 +0900

    importの中にreactiveを記述

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,26 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 const title = ref('Vue.jd Course')
 let price = ref(9.99)
 console.log(price.value)
 function increment() {
   price.value++
-  console.log(price.value)
+  instructor.age++
 }
 const info = ref({
     students: 1000,
     rating: 4.0
 })
-console.log(info.value.students)
+const instructor = reactive({
+    name: 'Sei',
+    age: 25
+})
+console.log(instructor.age)
 </script>
 <template>
   <h1>Title: {{ title }}</h1>
   <h2>Price: ${{ price - 1 }}</h2>
   <button @click="increment">button</button>
   <h2>Students: {{ info.students }}</h2>
+  <h2>Instructor age: {{ instructor.age }}</h2>
 </template>
